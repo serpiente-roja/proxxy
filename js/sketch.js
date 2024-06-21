94737502db53fa673129c02e16592ba1eaf20d32
subjekt-iv <agustinprado007@gmail.com>
2024-06-20 21:03:01 -0300

    glitch fx js and responsive layout

--- js/sketch.js
+++ js/sketch.js
@@ -4,34 +4,39 @@ let filtro = true;
 let canvas;
 
 function preload() {
-  img = loadImage("../assets/" + Math.floor(Math.random() * 4 + 1) + ".png");
+  let imagePath = "../assets" + Math.floor(Math.random() * 4 + 1) + ".png";
+  console.log("Loading image:", imagePath);
+  img = loadImage(imagePath);
   imgb = img;
 }
 
 function setup() {
-
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
   canvas.style('z-index', '-1');
-
 }
 
 function mouseClicked() {
-  if (filtro == true) {
-    filtro = false;
-  } else {
-    filtro = true;
-  }
+  filtro = !filtro; // Toggle filter on mouse click
 }
 
 function draw() {
-  // image(imgb,0,0,width,height);
-  image(imgb, 400, 400, 400, 400);
-  for (var i = 0; i < 3; i++) {
-    copy(img, (int)(random(100)) + (int)(i * 100 * (mouseX / windowWidth)), 0, 100, img.height, (int)(i * 350 * (mouseX / windowWidth)), 0, 100, imgb.height);
+  background(255); // Clear background each frame
 
-    if (filtro == true) {
-      filter(INVERT);
+  // Calculate center position
+  let centerX = windowWidth / 2 - imgb.width / 2;
+  let centerY = windowHeight / 2 - imgb.height / 2;
+
+  // Display the image in the center of the screen
+  image(imgb, centerX, centerY);
+
+  // Your image manipulation code here
+  for (var i = 0; i < 3; i++) {
+    // Copying image parts
+    copy(img, int(random(100)) + int(i * 100 * (mouseX / windowWidth)), 0, 100, img.height, int(i * 350 * (mouseX / windowWidth)), 0, 100, imgb.height);
+
+    if (filtro) {
+      filter(INVERT); // Apply filter if required
     }
   }
 }
